b3e827a1c64a19b1028c96785312a0e90d2a5345
jihuen <23026390@myrp.edu.sg>
2025-01-07 14:57:06 +0800

    new

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import { useEffect, useState } from "react";
-const KEY = "";
+const KEY = "317782c8";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("batman");
 
-  fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=batman`)
-    .then((res) => res.json())
-    .then((data) => setMovies(data.Search));
+  useEffect(() => {
+    const controller = new AbortController();
+
+    fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.Response === "True") {
+          setMovies(data.Search);
+        } else {
+          setMovies([]); // Clear movies if no results
+        }
+      })
+      .catch((err) => {
+        if (err.name !== "AbortError") {
+          console.error("Fetch error:", err); // Log errors other than abort
+        }
+      });
+
+    return () => controller.abort(); // Cleanup function
+  }, [query]);
 
   return (
     <div>
       <h1>Movies</h1>
-      <input type="text" placeholder="Search movies..." />
+      <input
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
       <table>
         <thead>
           <tr>
@@ -20,12 +43,18 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {movies.map((movie) => (
-            <tr key={movie.imdbID}>
-              <td>{movie.Title}</td>
-              <td>{movie.Year}</td>
+          {movies.length > 0 ? (
+            movies.map((movie) => (
+              <tr key={movie.imdbID}>
+                <td>{movie.Title}</td>
+                <td>{movie.Year}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="2">No movies found.</td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
     </div>
